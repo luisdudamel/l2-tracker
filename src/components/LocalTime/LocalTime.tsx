4d5262ba6925f3504d811bc1ca5880a4a6f4be6c
Luis Dudamel <ladusa@gmail.com>
2024-02-14 19:08:59 +0100

    Add LocalTime test for rendering and local time

--- src/components/LocalTime/LocalTime.tsx
+++ src/components/LocalTime/LocalTime.tsx
@@ -1,43 +1,52 @@
 import { useEffect, useState } from "react";
 import { LocalTimeStyled } from "./LocalTimeStyled";
 
-const LocalTime = (): JSX.Element => {
-  const [currentTime, setCurrentTime] = useState<string>("");
+const LocalTime = () => {
+    const [currentTime, setCurrentTime] = useState<string>("");
 
-  useEffect(() => {
-    const updateCurrentTime = () => {
-      const localUserTime = new Date().toLocaleTimeString(getUserLocale(), {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      });
+    useEffect(() => {
+        const updateCurrentTime = () => {
+            const localUserTime = new Date().toLocaleTimeString(
+                getUserLocale(),
+                {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: false,
+                }
+            );
 
-      setCurrentTime(localUserTime);
+            setCurrentTime(localUserTime);
+        };
+
+        updateCurrentTime();
+
+        const currentSeconds = new Date().getSeconds();
+        const secondsUntilNextMinute = 60 - currentSeconds;
+
+        const intervalId = setInterval(
+            updateCurrentTime,
+            secondsUntilNextMinute * 1000
+        );
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const getUserLocale = (): string => {
+        if (navigator.languages && navigator.languages.length) {
+            return navigator.languages[0];
+        } else {
+            return navigator.language || "en";
+        }
     };
 
-    updateCurrentTime();
-
-    const intervalId = setInterval(updateCurrentTime, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  const getUserLocale = (): string => {
-    if (navigator.languages && navigator.languages.length) {
-      return navigator.languages[0];
-    } else {
-      return navigator.language || "en";
-    }
-  };
-
-  return (
-    <LocalTimeStyled>
-      <div className="local-time">
-        Your local time:{" "}
-        <span className="local-time__digits">{currentTime}</span>
-      </div>
-    </LocalTimeStyled>
-  );
+    return (
+        <LocalTimeStyled>
+            <p className="local-time">
+                Your local time:{" "}
+                <span className="local-time__digits">{currentTime}</span>
+            </p>
+        </LocalTimeStyled>
+    );
 };
 
 export default LocalTime;
--- src/components/LocalTime/LocalTimeStyled.ts
+++ src/components/LocalTime/LocalTimeStyled.ts
@@ -1,25 +1,26 @@
 import styled from "styled-components";
 
 export const LocalTimeStyled = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: flex-end;
-
-  .local-time {
     display: flex;
+    width: 100%;
     align-items: center;
-    gap: 1em;
-    height: 2em;
-    width: 50%;
-    color: black;
-    padding-left: 1em;
-    background-color: white;
-    border-radius: 0.2em;
-    font-weight: 700;
+    justify-content: flex-end;
 
-    &__digits {
-      color: #167420;
+    .local-time {
+        display: flex;
+        align-items: center;
+        gap: 1em;
+        height: 2em;
+        width: 50%;
+        max-width: 200px;
+        color: black;
+        padding-left: 1em;
+        background-color: white;
+        border-radius: 0.2em;
+        font-weight: 700;
+
+        &__digits {
+            color: #167420;
+        }
     }
-  }
 `;
