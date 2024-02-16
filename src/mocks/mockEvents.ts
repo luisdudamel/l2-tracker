ef09b22a74d5b0ba12a17b1689f8ad79b190b687
Luis Dudamel <ladusa@gmail.com>
2024-02-16 23:32:55 +0100

    Fix missing props on Header test

--- src/mocks/mockEvents.ts
+++ src/mocks/mockEvents.ts
@@ -5,12 +5,14 @@ export const mockRaidBosses: Event[] = [
         eventName: "Baium",
         key: "1",
         localTime: "23:00",
-        timeLeft: "15:00",
+        localTimeLeft: "15:00",
+        serverTime: "13:00",
     },
     {
         eventName: "Valakas",
         key: "2",
         localTime: "23:00",
-        timeLeft: "15:00",
+        localTimeLeft: "15:00",
+        serverTime: "13:00",
     },
 ];
