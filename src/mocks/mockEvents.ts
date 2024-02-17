4a49a73972deba76515cdb743dfe018643277018
Luis Dudamel <ladusa@gmail.com>
2024-02-17 22:00:04 +0100

    Fix mockRaidBosses types

--- src/mocks/mockEvents.ts
+++ src/mocks/mockEvents.ts
@@ -1,12 +1,14 @@
-import { Event } from "../types/events";
+import { EpicBossEvent } from "../types/events";
 
-export const mockRaidBosses: Event[] = [
+export const mockRaidBosses: EpicBossEvent[] = [
     {
         eventName: "Baium",
         key: "1",
         localTime: "23:00",
         localTimeLeft: "15:00",
         serverTime: "13:00",
+        eventDays: ["saturday"],
+        windowStart: "20:00",
     },
     {
         eventName: "Valakas",
@@ -14,5 +16,7 @@ export const mockRaidBosses: Event[] = [
         localTime: "23:00",
         localTimeLeft: "15:00",
         serverTime: "13:00",
+        eventDays: ["saturday"],
+        windowStart: "20:00",
     },
 ];
